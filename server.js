ecf88ba0bdf06a1b5e48e3a1614cf7ae18a2c0ea
Tamara Santamaria <58937301+marasortiz@users.noreply.github.com>
2025-06-10 11:22:02 +0000

    #01. Set up a Template Engine done

--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+// Configurar Pug como el motor de plantillas
+app.set('view engine', 'pug');
+// Especificar la carpeta de vistas (relativa al directorio del proyecto)
+app.set('views', './views/pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index'); // Renderiza views/pug/index.pug
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log('Usa el puerto ' + PORT);
 });
+
